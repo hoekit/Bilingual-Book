cded2867a0304a6cc12688f00f2ceb51ad5f7e5f
Hoe Kit CHEW <hoekit@gmail.com>
2021-04-14 20:39:51 +0700

    Add ability to redirect to sentence clicked

--- bin/Reader.pl
+++ bin/Reader.pl
@@ -2,9 +2,11 @@
 # PODNAME: Reader.pl
 # ABSTRACT: A reader app
 use Mojolicious::Lite;
+use lib qw(lib);
 use Book::Bilingual::Reader;
 
-my $reader = Book::Bilingual::Reader->new('t/ff01.mdown');
+my $file = 't/ff01.mdown';
+my $reader = Book::Bilingual::Reader->new($file);
 
 get '/' => sub {
     my $c = shift;
@@ -55,19 +57,22 @@ __DATA__
 
 <!DOCTYPE html>
 <html>
-  <head><title>Sebastians frameworks</title></head>
 <head>
-  <title>Bilingual Book Reader</title>
   <meta charset="UTF-8">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1, width=device-width">
+  <title>Bilingual Book Reader</title>
+
   <style>
-    body { margin:2rem; color:gray; line-height:1.8; }
+    body { margin:2rem; color:gray; line-height:1.8; font-size:1.5rem; }
     .chapter-number{ font-size:2rem; margin:0; }
     .chapter-title{ font-size:1.5rem; margin:0; }
     .paragraph-start{ margin-left:2rem; }
     .pointed{ color:#000; background-color:#FFFF00; padding:0.2rem 0;
         border-radius:5px; }
-    .src-lang{color:#000; background-color:#3687DE; padding:0.2rem 0; 
+    .src-lang{color:#FFF; background-color:#3687DE; padding:0.2rem 0;
         border-radius:5px; }
+    footer { position:fixed; width:90%; left:0; bottom:0; text-align:right;
+             background-color:#FFF }
   </style>
 </head>
   <body>
@@ -75,8 +80,11 @@ __DATA__
 
     <br/><br/><br/>
 
+    <footer>
     %== $prev_html
+    |
     %== $next_html
+    </footer>
 
   </body>
 <script>
@@ -91,7 +99,7 @@ __DATA__
         if (elNext) { elNext.click() }
     }
 
-   document.onkeydown = function (event) {
+    document.onkeydown = function (event) {
       switch (event.keyCode) {
          case 37:
             console.log("Left key is pressed.");
@@ -108,7 +116,16 @@ __DATA__
             console.log("Down key is pressed.");
             break;
       }
-   };
+    };
+
+    // Redirect page to sentence that was clicked
+    document.onclick = function(e) {
+        let ptr = e.target.attributes["data-ptr"]
+        if (typeof ptr !== "undefined") {
+            let url = e.target.attributes["data-ptr"].value
+            window.location.href = "/reader/html?ptr="+url
+        }
+    };
 </script>
 
 </html>
