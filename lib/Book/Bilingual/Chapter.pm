1f1d84ca2c69f7d93967998a8b3ac73778bd4eae
Hoe Kit CHEW <hoekit@gmail.com>
2021-04-14 12:20:35 +0700

    Add some utlitily methods

--- lib/Book/Bilingual/Chapter.pm
+++ lib/Book/Bilingual/Chapter.pm
@@ -12,5 +12,18 @@ sub num_paragraphs {
 
     return scalar @{$self->{body}};
 }
+sub dlineset_at { ## ($i) :> Dlineset
+    my ($self,$i) = @_;
+    return $self->number if $i == 0;
+    return $self->title  if $i == 1;
+    return $self->body->[$i-2];
+}
+sub dlineset_count {
+    my ($self) = @_;
+
+    return (defined $self->number ? 1 : 0)
+           + (defined $self->title ? 1 : 0)
+           + scalar @{$self->{body}};
+}
 
 1;
